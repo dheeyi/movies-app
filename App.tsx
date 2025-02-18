d019f7dbc3e94572adeb557224b33f67cd0fe13b
dheeyi <dheeyi@gmail.com>
2025-02-18 18:37:07 -0400

    Component 2

--- App.tsx
+++ App.tsx
@@ -2,17 +2,38 @@ import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
+  StyleSheet,
+  View,
 } from 'react-native';
 import Home from './src/screens/Home/Home';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Home />
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={styles.scrollContent}
+      >
+        <View style={styles.centeredView}>
+          <Home />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContent: {
+    flexGrow: 1,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
